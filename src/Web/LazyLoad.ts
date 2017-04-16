422851a77d1efdd245d4dc35aec910b40d5c509d
Alexander Trauzzi <atrauzzi@gmail.com>
2017-04-16 07:52:00 -0500

    fix: Add a lazy script loader to the web platform.  Take 1.

---
+++ src/Web/LazyLoad.ts
@@ -0,0 +1,27 @@
+
+
+export async function lazyLoad(uri: string) {
+
+    let resolveDeferred: (event: Event) => void;
+    let rejectDeferred: (error: any) => void;
+    const deferred = new Promise<Event>((resolve, reject) => {
+        resolveDeferred = resolve;
+        rejectDeferred = reject;
+    });
+
+    const scriptTag = document.createElement("script");
+    scriptTag.src = uri;
+    scriptTag.type = "text/javascript";
+    scriptTag.async = true;
+    scriptTag.defer = true;
+    
+    scriptTag.onload = (event: Event) => {
+        
+        resolveDeferred(event);
+    };
+
+    document.getElementsByTagName("head")[0]
+        .appendChild(scriptTag);
+
+    return deferred;
+}
