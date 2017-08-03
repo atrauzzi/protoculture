a3588834b1c3055b977e7a4e3e8c2bd138dd882f
Alexander Trauzzi <atrauzzi@gmail.com>
2017-08-02 20:27:48 -0500

    fix: Web platforms will bind in redux debug tools when available.

--- src/Web/WebServiceProvider.ts
+++ src/Web/WebServiceProvider.ts
@@ -1,11 +1,20 @@
-import {ServiceProvider} from "../ServiceProvider";
-import {WebPlatform} from "./WebPlatform";
+import { ServiceProvider } from "../ServiceProvider";
+import { WebPlatform } from "./WebPlatform";
+import { reduxSymbols } from "../index";
+import { compose } from "redux";
 
+
+declare const window: {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any;
+};
 
 export class WebServiceProvider extends ServiceProvider {
 
     public async boot(): Promise<void> {
 
         this.bindPlatform(WebPlatform);
+
+        this.bundle.container.rebind(reduxSymbols.Compose)
+            .toConstantValue(window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
     }
 }
