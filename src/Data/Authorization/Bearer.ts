f455b3540fccca6b7615ad5c6f5c74c318239e26
Alexander Trauzzi <atrauzzi@gmail.com>
2018-12-04 11:50:24 -0600

    fix: Sweet overrides yo.

--- src/Data/Authorization/Bearer.ts
+++ src/Data/Authorization/Bearer.ts
@@ -18,6 +18,7 @@ declare module "../ApiConnection" {
 
     export interface ApiConnection<Configuration extends ConnectionConfiguration<any>> {
 
+        setAuthorization(type: AuthorizationType.Bearer, authorization: BearerAuthorization): void;
         createAxiosBearerAuthorizationConfiguration(authorization: BearerAuthorization): Promise<Partial<AxiosRequestConfig>>;
     }
 }
